b0db06a014858d4599a354dc80f5f9a549f26271
Drew Krehel <drewkrehel@gmail.com>
2025-03-19 23:43:30 +0000

    View templates

--- dice.rb
+++ dice.rb
@@ -9,10 +9,6 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 
-get("/howdy") do
-  return "Hello, world!"
-end
-
 get("/") do
   luck_num = rand(100)
   "
@@ -20,8 +16,8 @@ get("/") do
   <ul>
     <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
     <li><a href=\"/dice/3/8\">Roll three 8-sided dice</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a><li>
-
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+    <li><a href=\"/dice/8/20\">Roll eight 20-sided dice</a></li>
   </ul>
   <p>Luck # = #{luck_num}</p>
   "
@@ -59,11 +55,21 @@ get("/dice/3/8") do
 end
 
 get("/dice/5/4") do
-  dice = Array.new(5) {rand(1.4)}
+  dice = Array.new(5) {rand(1..4)}
   sum = dice.sum
 
-  outcome = "You rolled #{dice.join(',')}"
+  outcome = "You rolled #{dice.join(', ')} for a total of #{sum}."
 
   "<h1>5d4</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/8/20") do
+  dice = Array.new(8) {rand(1..20)}
+  sum = dice.sum
+
+  outcome = "You rolled #{dice.join(', ')} for a total of #{sum}."
+
+  "<h1>8d20</h1>
   <p>#{outcome}</p>"
 end
