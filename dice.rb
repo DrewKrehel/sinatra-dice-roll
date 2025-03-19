f92b5f143924f69bf5c37549dd12e5e70f9b24f4
Drew Krehel <drewkrehel@gmail.com>
2025-03-19 01:13:06 +0000

    problem 2

--- dice.rb
+++ dice.rb
@@ -20,8 +20,8 @@ get("/") do
   "
   <h1>Dice Roll</h1>
   <ul>
-    <li><a href=\"/dice/2/6">Roll two 6-sided dice</a></li>
-    <li><a herf=\"/dice/3/8">Roll three 8-sided dice</a></li>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a herf=\"/dice/3/8\">Roll three 8-sided dice</a></li>
     
   </ul>
   "
