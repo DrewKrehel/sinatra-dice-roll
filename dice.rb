72f22eb9c7e4869c69160d763e971ae71011854e
Drew Krehel <drewkrehel@gmail.com>
2025-03-19 01:02:44 +0000

    problem 1

--- dice.rb
+++ dice.rb
@@ -1,5 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
 get("/howdy") do
   return "Hello, world!"
 end
@@ -11,9 +20,11 @@ get("/") do
   "
   <h1>Dice Roll</h1>
   <ul>
-    <li><a href=\"
+    <li><a href=\"/dice/2/6">Roll two 6-sided dice</a></li>
+    <li><a herf=\"/dice/3/8">Roll three 8-sided dice</a></li>
+    
+  </ul>
   "
-  
 end
 
 get("/zebra") do
@@ -33,4 +44,16 @@ get("/dice/2/6") do
 	
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
+end
+
+get("/dice/3/8") do
+  first_die = rand(1..8)
+  second_die = rand(1..8)
+  third_die = rand(1..8)
+  sme = first_die + second_die + third_die
+
+  outcome = "You rolled a #{first_die}, a #{second_die}, and a #{third_die} for a total of #{sum}."
+
+  "<h1>3d8</h1>
+  <p>#{outcome}</p>"
 end
