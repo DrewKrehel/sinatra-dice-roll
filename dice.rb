cdc27acf418417f5a9bacc86e31c649eb7aec21b
Drew Krehel <drewkrehel@gmail.com>
2025-03-20 02:41:49 +0000

    Silent ERB tegs

--- dice.rb
+++ dice.rb
@@ -24,13 +24,13 @@ end
 # end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @first_die = rand(1..6)
+  @second_die = rand(1..6)
+  @sum = @first_die + @second_die
 	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{@first_die} and a #{@second_die} for a total of #{@sum}."
 	
-  erb(:two_six, {:layout => :wrapper})
+  erb(:two_six)
 end
 
 get("/dice/3/8") do
@@ -41,7 +41,7 @@ get("/dice/3/8") do
 
   @outcome = "You rolled a #{first_die}, a #{second_die}, and a #{third_die} for a total of #{sum}."
 
-  erb(:three_eight, {:layout => :wrapper})
+  erb(:three_eight)
 end
 
 get("/dice/5/4") do
@@ -50,14 +50,26 @@ get("/dice/5/4") do
 
   @outcome = "You rolled #{dice.join(', ')} for a total of #{sum}."
 
-  erb(:five_four, {:layout => :wrapper})
+  erb(:five_four)
 end
 
 get("/dice/8/20") do
-  dice = Array.new(8) {rand(1..20)}
-  sum = dice.sum
+  @dice = Array.new(8) {rand(1..20)}
+  @sum = @dice.sum
 
-  @outcome = "You rolled #{dice.join(', ')} for a total of #{sum}."
+  @outcome = "You rolled #{@dice.join(', ')} for a total of #{@sum}."
 
-  erb(:eight_twenty, {:layout => :wrapper})
+  erb(:eight_twenty)
+end
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six)
 end
